ab025e84a77234c6b995324f8db3f60299e8c6c3
Nicon <65840719+UKNOWN2@users.noreply.github.com>
2020-07-30 19:31:02 +0530

    Update t-edit.sh

--- t-edit.sh
+++ t-edit.sh
@@ -1,11 +1,11 @@
 pkg install git
 git clone https://github.com/Bhai4You/Termux-Banner
-cd $HOME/Termux-Banner
+cd $HOME/Termux-Editor/Termux-Banner
 chmod +x requirement.sh
 chmod +x t-ban.sh
 bash requirement.sh
-cd $HOME/Termux-Banner
+cd $HOME/Termux-Editor/Termux-Banner
 bash t-ban.sh
-cd $HOME
+cd $HOME/Termux-Editor
 rm -rf Termux-Banner
 clear
