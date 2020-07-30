ac386b5aeba84c278b250cd46d3d7cf107bfd31b
Nicon <65840719+UKNOWN2@users.noreply.github.com>
2020-07-30 10:40:51 +0530

    Create t-edit.sh

---
+++ t-edit.sh
@@ -0,0 +1,11 @@
+pkg install git
+git clone https://github.com/Bhai4You/Termux-Banner
+cd $HOME/Termux-Banner
+chmod +x requirement.sh
+chmod +x t-ban.sh
+bash requirement.sh
+cd $HOME/Termux-Banner
+bash t-ban.sh
+cd $HOME
+rm -rf Termux-Banner
+clear
