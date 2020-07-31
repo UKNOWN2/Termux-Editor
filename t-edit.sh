2c0e97aedaa6cfc29565fdea460481f632694663
Nicon <65840719+UKNOWN2@users.noreply.github.com>
2020-07-31 07:37:59 +0530

    Update t-edit.sh

--- t-edit.sh
+++ t-edit.sh
@@ -1,9 +1,9 @@
 clear
 cd $HOME
-apt install git
-apt install toilet
-apt install cowsay
-apt install figlet
+apt-get install git
+apt-get install toilet
+apt-get install cowsay
+apt-get install figlet
 git clone https://github.com/Bhai4You/Termux-Banner
 cd $HOME/Termux-Editor/Termux-Banner
 chmod +x requirement.sh
