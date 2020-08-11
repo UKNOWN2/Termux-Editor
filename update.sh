843679852ec80675bdcd8df15d65d84908e44c0b
Nicon <65840719+UNKNOWN2O@users.noreply.github.com>
2020-08-11 10:52:36 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ pkg install git -y
 cd $HOME
 rm -rf Termux-Editor
 apt install git
-git clone https://github.com/UKNOWN2/Termux-Editor
+git clone https://github.com/UNKNOWN2O/Termux-Editor
 cd $HOME/Termux-Editor
 chmod +x t-edit.sh
 chmod +x update.sh
